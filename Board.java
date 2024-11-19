cbdc1300553917541d376a9396f255448a1fbf57
alex <evesal01@gmail.com>
2024-11-19 12:40:50 -0700

    create Board.java

---
+++ Board.java
@@ -0,0 +1,11 @@
+/*
+ * File: Board.java
+ * Authors: Elliott Cepin, Yashi Gupta, Aarush Parvataneni, Alex Salgado
+ * 
+ * This class represents the board for our 2048. It has a default size 4x4.
+ */
+
+
+public class Board {
+
+}
